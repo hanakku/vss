5dd275d384e33dd4ca3810e930ce2f35b5dec7b1
aimfo <aimfo@192.168.219.102>
2024-07-14 18:19:18 +0900

    2nd

--- vss_ver4.2/src/main/java/jvbz/boot/config/DatabaseTestRunner.java
+++ vss_ver4.2/src/main/java/jvbz/boot/config/DatabaseTestRunner.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseTestRunner implements CommandLineRunner {
-
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
